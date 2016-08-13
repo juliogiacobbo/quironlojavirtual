7f88b1d2378f65fee8472b5e8c85b73674175eb5
juliogiacobbo <juliogiacobbo@gmail.com>
2016-08-13 11:54:14 -0300

    Webconfig connection

---
+++ Quiron.LojaVirtual.Dominio/Repositorio/EfDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Quiron.LojaVirtual.Dominio.Entidades;
+
+namespace Quiron.LojaVirtual.Dominio.Repositorio
+{
+	public class EfDbContext : DbContext
+	{
+		//Mapeamento da classe Produtos que criamos no Entity
+		public DbSet<Produto> Produtos { get; set; }
+	}
+}
